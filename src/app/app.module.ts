0a6f2e551b246be0eb9567b922c5771242b6ff67
Rahul Sachdeva <rahul.sachdeva@joshtechnologygroup.com>
2026-02-06 08:16:55 +0530

    RS_FE_02: Updated aliases, styles as per BEM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { SharedModule } from '@shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
