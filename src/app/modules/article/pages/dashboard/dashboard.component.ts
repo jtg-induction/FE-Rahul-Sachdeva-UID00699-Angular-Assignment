e7a9861b9bc4bcb44a4021062ebc32cb11c87209
Rahul Sachdeva <rahul.sachdeva@joshtechnologygroup.com>
2026-02-07 16:39:42 +0530

    rebased RS_FE_03

--- src/app/modules/article/pages/dashboard/dashboard.component.ts
+++ src/app/modules/article/pages/dashboard/dashboard.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {}
