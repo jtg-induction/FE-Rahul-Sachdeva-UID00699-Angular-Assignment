824291779ec0f045da9566bf03b5971ce7ca8aeb
Rahul Sachdeva <rahul.sachdeva@joshtechnologygroup.com>
2026-02-07 15:10:48 +0530

    Updated TS Docs and corrected Routes, updated styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
 import { GlobalErrorHandler } from '@core/handlers/global-error.handler';
 import { SharedModule } from '@shared/shared.module';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
