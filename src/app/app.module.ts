f218a9ac3336adceb9297490af3c47a7adc5b171
Rahul Sachdeva <rahul.sachdeva@joshtechnologygroup.com>
2026-02-11 10:05:52 +0530

    RS_FE_03: Updated Auth Validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { GlobalErrorHandler } from '@core/handlers/global-error.handler';
 import { SharedModule } from '@shared/shared.module';
 
-import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutComponent } from './core/components/layout/layout.component';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [
     provideAnimationsAsync(),
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { NavbarComponent } from './components/navbar';
 
 @NgModule({
   declarations: [NavbarComponent],
