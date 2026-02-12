01bf466174b2437c0caf30675b6c2d766114428b
Rahul Sachdeva <rahul.sachdeva@joshtechnologygroup.com>
2026-02-12 13:22:42 +0530

    RS_FE_02: Found Fixes

--- src/app/modules/not-found/not-found.component.ts
+++ src/app/modules/not-found/not-found.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,8 +9,12 @@ import { Router } from '@angular/router';
 })
 export class NotFoundComponent {
   private router = inject(Router);
+  private http = inject(HttpClient);
 
   goHome(): void {
+    this.http.get('/images/dog.jpg', {responseType: 'arraybuffer'}).subscribe((buffer) => {
+  console.log('The image is ' + buffer.byteLength + ' bytes large');
+});
     this.router.navigate(['/']);
   }
 }
